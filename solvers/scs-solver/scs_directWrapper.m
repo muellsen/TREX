8d420706864a3d78d7ad3b091412263dc4152082
Christian L. Müller <cmueller@simonsfoundation.org>
2020-12-04 13:00:23 +0100

    included files for scs solver

---
+++ solvers/scs-solver/scs_directWrapper.m
@@ -0,0 +1,16 @@
+function [x, y, s, info] = scs_directWrapper(A,b,c,x,y,s, cone, params)
+% Wrapper for the parfor implementation of scs
+
+data.A = A;
+data.b = b;
+data.c = c;
+
+% Warm start solutions used over c-path
+if ~isempty(x)
+    data.x = x;
+    data.y = y;
+    data.s = s;
+end
+
+[x, y, s, info] = scs_direct(data, cone, params);
+
